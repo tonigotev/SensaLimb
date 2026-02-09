a4a0d6643587f64109ef298c156d7c606a9b3715
tonigotev <antong318@gmail.com>
2026-02-09 20:47:07 +0200

    UART DMA + IDLE interrupt integration template.

--- NUCLEO/main.c
+++ NUCLEO/main.c
@@ -3,6 +3,14 @@
 #include "ad7606.h"
 #include "ad7606_pins.h"
 #include "core_cm33.h"
+#include "UART/nucleo_uart_rx_port.h"
+
+// Provided by stm32u5xx_it.c
+void uart_dma_idle_init(UART_HandleTypeDef *huart);
+
+#define SAMPLE_RATE 2000
+#define SAMPLES 200
+#define CHANNELS 8
 
 static inline void dwt_init(void) {
     CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
@@ -20,25 +28,29 @@ void main(void) {
     dwt_init();
     ad7606_init();
 
-    uint16_t channels[2000][DEFINED_CHANNELS];
+    // Start UART DMA + IDLE handling
+    uart_dma_idle_init(&huartX);
+
+    uint16_t channels[SAMPLE_RATE][CHANNELS];
     // 2khz sampling rate on 180mhz clock
     // 180mhz / 2khz = 90k cycles per sample
     // 1/180mhz = 5.56ns
     // 90k * 5.56ns = 500.4us
     uint32_t period = convert_us_to_cycles(500);
     uint32_t t = DWT->CYCCNT;
-    for (int i = 0; i < 2000; i++) {
-        ad7606_read_all_channels(channels[i], DEFINED_CHANNELS);
+    for (int i = 0; i < SAMPLE_RATE; i++) {
+        ad7606_read_all_channels(channels[i], CHANNELS);
+        nucleo_uart_rx_port_process();
         while(DWT->CYCCNT - t < period){}
         t += period;
     }
 
     // We downsample by 10 to get 200 samples
     // We average the samples to get the RMS
-    float sum[DEFINED_CHANNELS] = {0};
-    float rms[200][DEFINED_CHANNELS] = {0};
-    for (int g = 0; g < 200; g++) {
-        for (int i = 0; i < DEFINED_CHANNELS; i++) {
+    float sum[CHANNELS] = {0};
+    float rms[SAMPLES][CHANNELS] = {0};
+    for (int g = 0; g < SAMPLES; g++) {
+        for (int i = 0; i < CHANNELS; i++) {
             for (int j = 0; j < 10; j++) {
                 float v = (float)channels[g * 10 + j][i];
                 sum[i] += v * v;
